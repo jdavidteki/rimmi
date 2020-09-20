e123582a0407a1c5eaad7654106a955002a33e1f
= <jdavid@yext.com>
2020-09-20 07:09:36 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/rimmi/precache-manifest.d2586131e5292777bcefc434e8e1438c.js"
+  "/rimmi/precache-manifest.8ce7979f37690ee1fd92b8ae7cde32f0.js"
 );
 
 workbox.skipWaiting();
